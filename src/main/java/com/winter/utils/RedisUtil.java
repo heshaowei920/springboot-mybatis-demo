3ccce86a9ad6cf5618ae1f0e27100b6aaf8e2729
heshaowei <heshaowei@zywamail.com>
2019-09-04 15:42:46 +0800

    增加redis分布式锁

--- src/main/java/com/winter/utils/RedisUtil.java
+++ src/main/java/com/winter/utils/RedisUtil.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -123,8 +124,8 @@ public class RedisUtil {
     /**
      * 递增
      *
-     * @param key 键
-     * @param delta  要增加几(大于0)
+     * @param key   键
+     * @param delta 要增加几(大于0)
      * @return
      */
     public long incr(String key, long delta) {
@@ -137,8 +138,8 @@ public class RedisUtil {
     /**
      * 递减
      *
-     * @param key 键
-     * @param delta  要减少几(小于0)
+     * @param key   键
+     * @param delta 要减少几(小于0)
      * @return
      */
     public long decr(String key, long delta) {
@@ -548,6 +549,52 @@ public class RedisUtil {
         } catch (Exception e) {
             e.printStackTrace();
             return 0;
+        }
+    }
+
+    //==========================分布式锁===============================
+
+    /**
+     * 加锁
+     *
+     * @param key   被秒杀商品的id
+     * @param value 当前线程操作时的 System.currentTimeMillis() + 2000，2000是超时时间，这个地方不需要去设置redis的expire，
+     *              也不需要超时后手动去删除该key，因为会存在并发的线程都会去删除，造成上一个锁失效，结果都获得锁去执行，并发操作失败了就。
+     * @return
+     */
+    public boolean lock(String key, String value) {
+        //如果key值不存在，则返回 true，且设置 value
+        if (redisTemplate.opsForValue().setIfAbsent(key, value)) {
+            return true;
+        }
+
+        //获取key的值，判断是是否超时
+        String curVal = redisTemplate.opsForValue().get(key).toString();
+        if (!StringUtils.isEmpty(curVal) && Long.parseLong(curVal) < System.currentTimeMillis()) {
+            //获得之前的key值，同时设置当前的传入的value。这个地方可能几个线程同时过来，但是redis本身天然是单线程的，所以getAndSet方法还是会安全执行，
+            //首先执行的线程，此时curVal当然和oldVal值相等，因为就是同一个值，之后该线程set了自己的value，后面的线程就取不到锁了
+            String oldVal = redisTemplate.opsForValue().getAndSet(key, value).toString();
+            if (!StringUtils.isEmpty(oldVal) && oldVal.equals(curVal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解锁
+     *
+     * @param key
+     * @param value
+     */
+    public void unlock(String key, String value) {
+        try {
+            String curVal = redisTemplate.opsForValue().get(key).toString();
+            if (!StringUtils.isEmpty(curVal) && curVal.equals(value)) {
+                redisTemplate.opsForValue().getOperations().delete(key);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
